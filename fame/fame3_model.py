f705b98bf2785984295165caeb3a1dbed97e12a1
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-24 23:14:44 +1300

    Provide input to fame as sdf to simplify atom mapping

--- fame/fame3_model.py
+++ fame/fame3_model.py
@@ -6,14 +6,7 @@ from typing import List, Tuple
 
 import pandas as pd
 from nerdd_module import Problem, SimpleModel
-from rdkit.Chem import (
-    KekulizeException,
-    Mol,
-    MolFromSmiles,
-    MolToSmiles,
-    SanitizeMol,
-    SmilesParserParams,
-)
+from rdkit.Chem import KekulizeException, Mol, SanitizeMol, SDWriter
 from sh import Command
 
 from .resources import get_fame3_executable
@@ -42,22 +35,27 @@ default_columns = [
 ]
 
 
-def predict_mols(mols: List[Mol], mode="P1+P2"):
+def predict_mols(mols: List[Mol], mode="P1+P2") -> List[dict]:
     with TemporaryDirectory() as tmp_dir:
         output_dir = os.path.join(tmp_dir, "output")
 
-        # Note 1: For simplicity, we always provide the input for Fame3 in SMILES format (even if
-        #         the input was given as a mol block / sdf).
-        # Note 2: In order to be consistent with the original implementation, SMILES have to be
-        #         kekulized and all hydrogens have to be explicit. This changes the atom indices
-        #         and we have to map the results back to the original atom indices (see below).
-        input_smiles = [
-            MolToSmiles(m, canonical=False, allHsExplicit=True, kekuleSmiles=True)
-            for m in mols
-        ]
+        # For simplicity, we always provide the input for Fame3 in SDF format (even if the input
+        # was given as SMILES).
+        input_sdf = os.path.join(tmp_dir, "input.sdf")
+        writer = SDWriter(input_sdf)
+        try:
+            for i, mol in enumerate(mols):
+                # We need to assign proper names to the molecules, otherwise Fame3 will fail.
+                # Also, we can later use the names to map the results back to the input molecules.
+                copy = Mol(mol)
+                copy.SetProp("_Name", f"mol_{i}")
+
+                writer.write(copy)
+        finally:
+            writer.close()
 
         # execute Fame3 on the list of smiles
-        fame3("-s", *input_smiles, o=output_dir, m=mode)
+        fame3(input_sdf, o=output_dir, m=mode)
 
         # Fame3 creates the following result file structure:
         #
@@ -105,46 +103,6 @@ def predict_mols(mols: List[Mol], mode="P1+P2"):
             # round all numbers to 3 decimal places
             for col in ["probability_0", "probability_1", "ad_score"]:
                 df[col] = df[col].round(3)
-
-            # Create the mapping that maps atom indices in the manipulated molecules (given as
-            # "input_smiles" to Fame; has hydrogens; is kekulized) to the atom indices in the
-            # original molecules (given as "mols" to this method).
-            mapping = {}
-            for i, mol, input_smi in zip(range(len(mols)), mols, input_smiles):
-                # If finding an alignment fails, we keep the original atom indices.
-                try:
-                    # Copy the molecule to avoid changes in the original molecule (GetSubstructMatch
-                    # changes marked atoms inplace).
-                    preprocessed_mol = Mol(mol)
-
-                    # Get the manipulated molecule and keep the hydrogens (removeHs=False).
-                    params = SmilesParserParams()
-                    params.removeHs = False
-                    params.sanitize = True
-                    fame_mol = MolFromSmiles(input_smi, params)
-
-                    # Use the graph matching algorithm to find the atom mapping. The mapping is a tuple
-                    # of atom indices. When index j maps to number k, then atom j in fame_mol is atom k
-                    # in the original mol.
-                    match = preprocessed_mol.GetSubstructMatch(fame_mol)
-
-                    # Make sure that all heavy atoms are matched.
-                    assert len(match) == preprocessed_mol.GetNumAtoms()
-
-                    # The expression mapping[m][i] maps atom i in fame_mol m to the corresponding atom
-                    # in the original mol.
-                    mapping[i] = match
-                except Exception as e:
-                    logger.error(e)
-                    mapping[i] = list(range(mol.GetNumAtoms()))
-
-            # apply the mapping
-            df["atom_id"] = df.apply(
-                lambda row: mapping[row["mol_id"]][row["atom_id"]], axis=1
-            )
-            df["atom"] = df.apply(
-                lambda row: f"{row['atom'].split('.')[0]}.{row['atom_id']}", axis=1
-            )
 
             # filter and reorder columns
             df = df[default_columns]
